fe274136c1af86f298c0668cf22635f4b842e249
Yuki <yuki.mul.tiplus@gmail.com>
2018-04-08 01:28:19 +0900

    Remove sample HelloWorld.vue

--- app-client/src/router/index.js
+++ app-client/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Main from '@/components/Main'
 
 Vue.use(Router)
@@ -9,11 +8,6 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/test',
       name: 'Main',
       component: Main
     }
