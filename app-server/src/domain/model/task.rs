085a9b15f043e439baf514d3e23c7aa3ce4a8b0c
Yuki <yuki.mul.tiplus@gmail.com>
2018-04-08 01:24:44 +0900

    Modify preflight settings (PUT -> PATCH)

--- app-server/src/domain/model/task.rs
+++ app-server/src/domain/model/task.rs
@@ -32,7 +32,7 @@ impl<'r> Responder<'r> for Task {
         Response::build()
             .raw_header("Content-Type", "application/json")
             .raw_header("Access-Control-Allow-Origin", "*")
-            .raw_header("Access-Control-Allow-Methods", "GET,POST,PUT,HEAD,OPTIONS")
+            .raw_header("Access-Control-Allow-Methods", "GET,POST,PATCH,HEAD,OPTIONS")
             .raw_header("Access-Control-Allow-Headers", "Content-Type")
             .raw_header("Access-Control-Allow-Credentials", "true")
             .sized_body(Cursor::new(format!(
@@ -59,7 +59,7 @@ impl<'r> Responder<'r> for TaskList {
         Response::build()
             .raw_header("Content-Type", "application/json")
             .raw_header("Access-Control-Allow-Origin", "*")
-            .raw_header("Access-Control-Allow-Methods", "GET,POST,PUT,HEAD,OPTIONS")
+            .raw_header("Access-Control-Allow-Methods", "GET,POST,PATCH,HEAD,OPTIONS")
             .raw_header("Access-Control-Allow-Headers", "Content-Type")
             .raw_header("Access-Control-Allow-Credentials", "true")
             .sized_body(Cursor::new(format!(
